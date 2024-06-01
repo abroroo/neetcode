73a3a79f569066a4c7c55e99f56d5b8ba9c8f7c7
Adam <adam@imagoworks.ai>
2024-06-01 16:26:02 +0900

    Valid Anagram is solved

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,5 +1,5 @@
 
 S
-#Arrays_Hashing/ContainsDuplicate.js,1/2/127c832ae90303ec64ec36e41aa4c68c5d0c2a0a
-S
 #Arrays_Hashing/ContainsDuplicate.ts,8/c/8c74cfb5299612b22eee44bec508d12f24624f5f
+N
+Arrays_Hashing/ValidAnagram.ts,d/d/dd34a3ad4f73046f1c8143f619e83e508bff9c34
---
+++ Arrays_Hashing/ValidAnagram.ts
@@ -0,0 +1,15 @@
+function isAnagram(s: string, t: string): boolean {
+    if (s.length != t.length) return false
+    let hashS = new Map<string, number>();
+    let hashT = new Map<string, number>();
+
+    Array.from(s).map((i: string) => hashS.set(i, hashS.has(i) ? (hashS.get(i) + 1) : 1))
+    Array.from(t).map((i: string) => hashT.set(i, hashT.has(i) ? (hashT.get(i) + 1) : 1))
+
+    for(let i = 0; i < t.length; i++){
+        if (hashT.get(t[i]) != hashS.get(t[i])) {
+            return false
+        }
+    }
+    return true
+};
